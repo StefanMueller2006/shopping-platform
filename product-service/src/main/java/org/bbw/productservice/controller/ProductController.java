ab43ed12f1f16eacd9290af58c960f350f7e587d
aaronrhyner <aaron.rhyner@lernende.bbw.ch>
2025-01-26 18:05:56 +0100

    add product-service to gateway-service

--- product-service/src/main/java/org/bbw/productservice/controller/ProductController.java
+++ product-service/src/main/java/org/bbw/productservice/controller/ProductController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
+@CrossOrigin("http://localhost:8080")
 public class ProductController {
 
     @Autowired
