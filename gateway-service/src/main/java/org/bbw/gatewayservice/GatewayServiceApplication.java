2fd4fb696cc39f01b878ef50e619c3244b6374b8
Stefan Thomas Müller <stefan.mueller@lernende.bbw.ch>
2025-01-26 13:13:42 +0100

    Add Circuit Breaker with fallback response

--- gateway-service/src/main/java/org/bbw/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/org/bbw/gatewayservice/GatewayServiceApplication.java
@@ -5,21 +5,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
 public class GatewayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p
-						.path("/products")
-						.filters(f -> f.addRequestHeader("Hello", "World"))
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/products")
+                        .filters(f -> f.circuitBreaker(config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
 						.uri("http://localhost:8081"))
 				.build();
-	}
+    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
 }
