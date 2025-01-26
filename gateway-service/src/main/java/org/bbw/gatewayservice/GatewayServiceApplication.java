6c61d521900f830d708b8f89f96e0a41c4849d92
Stefan Thomas Müller <stefan.mueller@lernende.bbw.ch>
2025-01-26 13:04:40 +0100

    Add simple gateway service

---
+++ gateway-service/src/main/java/org/bbw/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,25 @@
+package org.bbw.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/products")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://localhost:8081"))
+				.build();
+	}
+}
